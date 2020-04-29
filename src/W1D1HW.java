aba770d98928b9da8047777e39004581dd6f0802
Teresa Malave <dev.malave7@gmail.com>
2020-04-28 22:19:57 -0500

    multiply hw commit test 1

---
+++ src/W1D1HW.java
@@ -0,0 +1,10 @@
+public class W1D1HW {
+    public static void main(String args[]) {
+        int i=9;
+        int j=8;
+
+        //The multiplication of the two operands
+        System.out.println("The multiplication of the two operands is " + (i * j));
+
+    }
+}
