c547414e45a831d100c9b442356ad124e0b71ff7
Teresa Malave <dev.malave7@gmail.com>
2020-04-30 00:36:24 -0500

    understanding scanner a bit better. added resources using to get homework complete

--- src/unitconversion.java
+++ src/unitconversion.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 //https://www.geeksforgeeks.org/scanner-class-in-java/
+//resource: https://www.udemy.com/course/java-for-absolute-beginners/learn/lecture/9734854#content
 
 public class unitconversion {
     public static void main(String[] args)  {
-
+        //this is creating "scanner object" that we need to use for this program; telling scanner to read from our systems input(standard input) through System.in inside the ()
+        Scanner a = new Scanner(System.in);
     }
 }
