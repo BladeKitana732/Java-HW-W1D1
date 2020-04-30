1fea91086081c41b0be35bb2ba0230d77088d763
Teresa Malave <dev.malave7@gmail.com>
2020-04-30 00:49:16 -0500

    adding comment

--- src/unitconversion.java
+++ src/unitconversion.java
@@ -6,5 +6,8 @@ public class unitconversion {
     public static void main(String[] args)  {
         //this is creating "scanner object" that we need to use for this program; telling scanner to read from our systems input(standard input) through System.in inside the ()
         Scanner a = new Scanner(System.in);
+
+        //Code must convert 1 inch to every 0.0254 meter (ex. 1000.0 inches => 25.4 meters)
+        int num1 =
     }
 }
